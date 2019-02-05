db30b36ab1d1966677f563b5ceb3ab08b342eacf
John Henckel <henckel.jonathan@mayo.edu>
2019-02-04 22:04:07 -0600

    better style

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
-import axios from 'axios';
+import './common.scss';
 import { Utils } from './utils';
-
-// The window.app is a global pointer to the singleton of this class.
-// This app main job is to be the container for all the child view/controllers.
-// Also this class manages switching context from one view to another.
 
 export class MainApp {
 
@@ -19,5 +15,4 @@ export class MainApp {
 
 let app = new MainApp(new Utils(document));
 
-// expose the app globally, so we can use it in the index.html
 (<any>window || global || {}).app = app;
